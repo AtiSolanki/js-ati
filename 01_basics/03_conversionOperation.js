7e4c20b8334c64bb47a2dd1c4d5e145a1e1c6feb
Atul Solanki <solankiati@gmail.com>
2025-11-21 03:50:54 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "hello"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isloggedIn = 1
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumer = 33
+
+let stringNumber = String(someNumer)
+console.log(stringNumber);
+console.log(typeof (stringNumber)) ;
