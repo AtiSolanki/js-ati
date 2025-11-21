cf2605671f1edb29d78af5fb6380014ee1ed112e
Atul Solanki <solankiati@gmail.com>
2025-11-21 03:20:13 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3)  // we are node.js, not using browser
+
+// console.log(3+3); console.log("hitesh");    // This is bad way of coding 
+
+/*console.log(3+
+    
+    3);  */  // This is also is bad way of coding
+
+
+    let name = "ravi" // string
+    let age = 18 // number
+    let isloggedIn = false // boolean trueor false
+
+    // DATA TYPES
+
+    /* 
+        number = 2 to power 53
+        bigint
+        string = "" or '' prefer ""
+        boolean = true/false
+        null = standalone value
+        undefined 
+        symbol = unique
+    */
+
+    // object
+
+
+    console.log(typeof age);
+    
