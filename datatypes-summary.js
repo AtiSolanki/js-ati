c14dd60a6788878af4da6b896aed73bf947d1736
Atul Solanki <solankiati@gmail.com>
2025-11-22 04:18:50 +0000

    datatypeSummary

---
+++ datatypes-summary.js
@@ -0,0 +1,27 @@
+// 1 # Primitive DataType
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, Bigint
+
+const score = 100
+const scorevalue = 100
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id  = Symbol('123')
+const anotherId  = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 334467456756756767678n  // Bigint
+
+
+/* _________________________________________________________________________________________________ */
+
+// Reference Non Primitive
+
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman", "naagraj", "doga"]
