e3abb42f47fecc3a8354b7dd265b8863afc01f43
Atul Solanki <solankiati@gmail.com>
2025-11-24 05:20:47 +0000

    nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,16 @@
+const score = 400 
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+// const outherNumber = 23.8966
+// const outherNumber = 123.8966
+const outherNumber = 1123.8966
+
+console.log(outherNumber.toPrecision(3));
