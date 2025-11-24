197a2d5c75b75d7eee9daf060744b8ee3f3df7a0
Atul Solanki <solankiati@gmail.com>
2025-11-24 07:38:31 +0000

    dates

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,37 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate); // its a object for interview queastion
+
+// let mYCratedDate = new Date(2025,1,11)
+// let mYCratedDate = new Date(2025,1,11 ,5,3)
+// let mYCratedDate = new Date("2023-01-14")
+let mYCratedDate = new Date("01-14-2023")
+// console.log(mYCratedDate.toDateString());
+// console.log(mYCratedDate.toLocaleString());
+
+
+let mYTimeStamp= Date.now()
+// console.log(mYTimeStamp);
+// console.log(mYCratedDate.getTime());
+// console.log(Math.floor(Date.now()/1000)); // count in second
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDate());
+// console.log(newDate.getDate()+1);
+// console.log(newDate.getDay());
+
+
+
+// newDate.toLocaleString('default',{
+//     weekday: "long",
+// })
